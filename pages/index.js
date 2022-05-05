0f75a49a1589f85324be7146b4ee603abcb62797
owenwilson <owenwilson76467963@gmail.com>
2022-05-05 01:21:14 -0400

    my portfolio personal

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my portfolio personal!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I am devops enginer! <code>pages/index.js</code>
         </p>
       </main>
 
